ed52242307c3eb88d2a726dda196a3ac0303b906
callejose642-boop <callejose642@gmail.com>
2026-07-08 02:30:08 -0500

    Delete SistemaBibliotecaMVC directory

--- SistemaBibliotecaMVC/js/alertas.js
+++
@@ -1,8 +0,0 @@
-// Alertas visuales de confirmación de acciones sensibles
-function confirmarEliminacion(evento) {
-    if (!confirm("¿Está completamente seguro de que desea eliminar este registro? Esta acción no se puede deshacer.")) {
-        evento.preventDefault();
-        return false;
-    }
-    return true;
-}
